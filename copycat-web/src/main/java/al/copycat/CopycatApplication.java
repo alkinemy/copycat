05bbf257a21372d74c4bc63148474acc83ab110a
joke111 <alta.second@gmail.com>
2017-04-12 20:55:15 +0900

    initialize

---
+++ copycat-web/src/main/java/al/copycat/CopycatApplication.java
@@ -0,0 +1,13 @@
+package al.copycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopycatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopycatApplication.class, args);
+	}
+
+}
