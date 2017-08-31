315e8f8a9c1295eceb2679e7689de65420e78a90
joke111 <alta.second@gmail.com>
2017-08-31 12:38:51 +0900

    몽고db 버림

--- copycat-web/src/main/java/al/copycat/domain/common/repository/CopycatRepository.java
+++ copycat-web/src/main/java/al/copycat/domain/common/repository/CopycatRepository.java
@@ -1,11 +1,11 @@
 package al.copycat.domain.common.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
 @NoRepositoryBean
-public interface CopycatRepository<T, ID extends Serializable> extends MongoRepository<T, ID>, QueryDslPredicateExecutor<T> {
+public interface CopycatRepository<T, ID extends Serializable> extends JpaRepository<T, ID>, QueryDslPredicateExecutor<T> {
 }
