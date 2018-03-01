a41d26382dc8ebecf0802871db52e1af79f64a83
joke111 <alta.second@gmail.com>
2018-03-01 22:19:08 +0900

    url download test view

--- copycat-admin/components/download/FileDownload.vue
+++ copycat-admin/components/download/FileDownload.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>SimpleFile download</h1>
-        <b-form @submit="onSubmit">
+        <b-form @submit="downloadFile">
             <b-form-file v-model="file" :state="Boolean(file)" placeholder="Choose a file..."></b-form-file>
             <b-button type="submit">Download</b-button>
         </b-form>
@@ -16,9 +16,8 @@
             }
         },
         methods: {
-            async onSubmit(event) {
+            async downloadFile(event) {
                 event.preventDefault();
-                console.log(this.file);
                 var data = new FormData();
                 data.append("file", this.file);
                 await this.$axios.$post(process.env.baseUrl + '/downloads/files', data)
--- copycat-admin/pages/index.vue
+++ copycat-admin/pages/index.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <FileDownload></FileDownload>
+        <UrlDownload></UrlDownload>
     </div>
 </template>
 
 <script>
-    import FileDownload from '../components/FileDownload.vue'
+    import FileDownload from '../components/download/FileDownload.vue'
+    import UrlDownload from '../components/download/UrlDownload.vue'
 
     export default {
         components: {
-            'FileDownload': FileDownload
+            'FileDownload': FileDownload,
+            'UrlDownload': UrlDownload,
         }
     }
 </script>
