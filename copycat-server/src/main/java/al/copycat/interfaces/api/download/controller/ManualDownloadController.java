f0ef3eab143ede5705effea8f4ad08225cf8f422
joke111 <alta.second@gmail.com>
2018-09-08 00:58:48 +0900

    return mono instead of blocking

--- copycat-server/src/main/java/al/copycat/interfaces/api/download/controller/ManualDownloadController.java
+++ copycat-server/src/main/java/al/copycat/interfaces/api/download/controller/ManualDownloadController.java
@@ -1,13 +1,17 @@
 package al.copycat.interfaces.api.download.controller;
 
+import al.copycat.interfaces.api.download.dto.MagnetDownloadDto;
 import al.copycat.interfaces.api.download.dto.MultipartFileDownloadDto;
 import al.copycat.interfaces.api.download.dto.TorrentDownloadDto;
-import al.copycat.interfaces.api.download.dto.MagnetDownloadDto;
 import al.copycat.interfaces.api.download.dto.UrlDownloadDto;
 import al.copycat.interfaces.api.download.service.DownloadApiFacadeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -21,31 +25,27 @@ public class ManualDownloadController {
 	}
 
 	@PostMapping("/downloads/files")
-	public void downloadFromFile(@ModelAttribute MultipartFileDownloadDto downloadDto) {
+	public Mono<Void> downloadFromFile(@ModelAttribute MultipartFileDownloadDto downloadDto) {
 		log.debug("Download file: {}", downloadDto);
-		downloadApiFacadeService.download(downloadDto)
-			.block();
+		return downloadApiFacadeService.download(downloadDto);
 	}
 
 	@PostMapping("/downloads/urls")
-	public void downloadUrl(@RequestBody UrlDownloadDto downloadDto) {
+	public Mono<Void> downloadUrl(@RequestBody UrlDownloadDto downloadDto) {
 		log.debug("Download url: {}", downloadDto);
-		downloadApiFacadeService.download(downloadDto)
-			.block();
+		return downloadApiFacadeService.download(downloadDto);
 	}
 
 	@PostMapping("/downloads/torrents/files")
-	public void downloadTorrent(@RequestBody TorrentDownloadDto downloadDto) {
+	public Mono<Void> downloadTorrent(@RequestBody TorrentDownloadDto downloadDto) {
 		log.debug("Download torrent: {}", downloadDto);
-		downloadApiFacadeService.download(downloadDto)
-			.block();
+		return downloadApiFacadeService.download(downloadDto);
 	}
 
 	@PostMapping("/downloads/torrents/magnets")
-	public void downloadMagnet(@RequestBody MagnetDownloadDto downloadDto) {
+	public Mono<Void> downloadMagnet(@RequestBody MagnetDownloadDto downloadDto) {
 		log.debug("Download magnet: {}", downloadDto);
-		downloadApiFacadeService.download(downloadDto)
-			.block();
+		return downloadApiFacadeService.download(downloadDto);
 	}
 
 }
